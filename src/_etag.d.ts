0aa42497239c3812ff604b700d1acb549954c440
Brian Takita <brian.takita@gmail.com>
2021-05-24 19:40:39 -0400

    ## 5.2.0
    ### Minor Changes
    
    - dist,types directory: addressing typescript build issues

--- src/_etag.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="node" />
-import { Stats } from 'fs';
-/**
- * Create a simple ETag.
- */
-export declare function _etag(entity: Stats, options: _etag_opts_type): string;
-export interface _etag_opts_type {
-    weak: boolean;
-}
